9e68d5f06861297cb420304cd1e0962e17f22aba
Yuki Takemoto <mottox2@users.noreply.github.com>
2025-07-13 23:10:59 +0900

    Update react-three-fiber

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,14 +1,13 @@
-// @ts-nocheck
 import React, { useRef, useEffect } from 'react';
-import { Canvas, useFrame, useThree } from 'react-three-fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Vector3 } from 'three';
-
+import * as THREE from 'three'
 
 function Box(props: any) {
   return (
     <mesh {...props}>
-      <boxBufferGeometry attach="geometry" args={props.geometry || [1, 1, 1]} />
-      <meshStandardMaterial attach="material" color={props.color || '#666600'} />
+      <boxGeometry args={props.geometry || [1, 1, 1]} />
+      <meshStandardMaterial color={props.color || '#666600'} />
     </mesh>
   )
 }
@@ -26,13 +25,23 @@ export function Background({count, mode, setRotation}: any) {
   const color = colors[count % 2]
   return (
     <div className='container'>
-      <Canvas shadowMap={true} camera={{ position: [3, 3, 3], fov: 90 }}>
-        <ambientLight />
-        <pointLight castShadow position={[0, 10, 20]} />
+      <Canvas
+        shadows
+        camera={{ position: [3, 3, 3], fov: 90 }}
+        // linear
+        gl={{
+          outputEncoding: THREE.sRGBEncoding,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          toneMappingExposure: 2,
+        }}
+      >
+        <ambientLight intensity={2} />
+        <directionalLight castShadow position={[0, 10, 20]} intensity={1.5} />
+        <directionalLight position={[0, 10, 100]} intensity={3} />
         <gridHelper args={[300, 100, 0x888888, 0x888888]} position={[0, -0.65, 0]}/>
         <mesh receiveShadow position={[0, -0.7, 0]}>
-          <boxBufferGeometry attach="geometry" args={[200, 0.1, 200]} />
-          <meshStandardMaterial attach="material" color={'#a0a0a0'} roughness={0.0} />
+          <boxGeometry args={[200, 0.1, 200]} />
+          <meshStandardMaterial color={'#d5d5d5'} roughness={0.0} />
         </mesh>
         <Sushi setRotation={setRotation} mode={mode} position={[0, 0, 0]} speed={0.03 * count} scale={[1 + 0.18 * count,1,1]} color={color} />
         <SetCameraLookAt />
@@ -58,7 +67,7 @@ const Sushi = ({speed, color, mode, setRotation, ...props}) => {
 
   return <group {...props} ref={group}>
       <Box position={[0, 0.7, 0]} geometry={[2, 0.4, 1]} castShadow color={color} />
-      <Box position={[0, 0, 0]} geometry={[2, 1, 1]} castShadow color="#CCCCCC" />
+      <Box position={[0, 0, 0]} geometry={[2, 1, 1]} castShadow color="#f5f5f5" />
     </group>
 }
 
